5c9266f75a18c7dcd75330b9e08a3de5dda9788d
DECATHLONHK <esther.lau@decathlon.com>
2026-01-31 16:36:19 +0800

    Create products.js

---
+++ products.js
@@ -0,0 +1,24 @@
+// Data extracted from your Excel
+const products = [
+  {
+    id: "8913928",
+    name_en: "Kiprun Kipnext",
+    name_zh: "男款路跑鞋 KipX",
+    price: 599,
+    tags: ["comfort"],
+    img: "https://contents.mediadecathlon.com/p8913928/sq/image.jpg",
+    url_en: "https://www.decathlon.com.hk/en-HK/p/8913928.html",
+    url_zh: "https://www.decathlon.com.hk/zh/p/8913928.html"
+  },
+  {
+    id: "8931228",
+    name_en: "KD900X LD2 Racing",
+    name_zh: "男士碳板路跑鞋 KD900X LD2",
+    price: 1299,
+    tags: ["performance"],
+    img: "https://contents.mediadecathlon.com/p8931228/sq/image.jpg",
+    url_en: "https://www.decathlon.com.hk/en-HK/p/8931228.html",
+    url_zh: "https://www.decathlon.com.hk/zh/p/8931228.html"
+  }
+  // 你可以按照這個格式繼續添加其他鞋款
+];

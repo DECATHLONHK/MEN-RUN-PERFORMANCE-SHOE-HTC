f5cc367cb9a1e8f08315d54bf4c2fc26f8d83a53
DECATHLONHK <esther.lau@decathlon.com>
2026-01-31 16:43:29 +0800

    Update products.js

--- products.js
+++ products.js
@@ -1,24 +1,32 @@
-// Data extracted from your Excel
 const products = [
   {
     id: "8913928",
-    name_en: "Kiprun Kipnext",
-    name_zh: "男款路跑鞋 KipX",
+    name_en: "Kiprun Kipnext Beige",
+    name_zh: "男款路跑鞋 KipX - 米色",
     price: 599,
-    tags: ["comfort"],
+    tags: ["comfort"], // 對應問卷的 'comfort'
     img: "https://contents.mediadecathlon.com/p8913928/sq/image.jpg",
     url_en: "https://www.decathlon.com.hk/en-HK/p/8913928.html",
     url_zh: "https://www.decathlon.com.hk/zh/p/8913928.html"
   },
   {
     id: "8931228",
-    name_en: "KD900X LD2 Racing",
-    name_zh: "男士碳板路跑鞋 KD900X LD2",
+    name_en: "KD900X LD2 Racing Orange",
+    name_zh: "男士碳板路跑鞋 KD900X LD2 - 橙色",
     price: 1299,
-    tags: ["performance"],
+    tags: ["performance"], // 對應問卷的 'performance'
     img: "https://contents.mediadecathlon.com/p8931228/sq/image.jpg",
     url_en: "https://www.decathlon.com.hk/en-HK/p/8931228.html",
     url_zh: "https://www.decathlon.com.hk/zh/p/8931228.html"
+  },
+  {
+    id: "8960535",
+    name_en: "Kiprun Kipride Light Grey",
+    name_zh: "男款路跑鞋 Kipride Light - 灰色",
+    price: 649,
+    tags: ["comfort"],
+    img: "https://contents.mediadecathlon.com/p8960535/sq/image.jpg",
+    url_en: "https://www.decathlon.com.hk/en-HK/p/8960535.html",
+    url_zh: "https://www.decathlon.com.hk/zh/p/8960535.html"
   }
-  // 你可以按照這個格式繼續添加其他鞋款
 ];
